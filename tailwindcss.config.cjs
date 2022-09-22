59f7311cc35db6c5cf8edceb154fc348fa8a0fd5
João Pedro <jpedro1580@gmail.com>
2022-09-22 07:54:29 -0300

    Add hero section

--- tailwindcss.config.cjs
+++ tailwindcss.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
         sans: ['Inter'],
         serif: ['Charis SIL'],
       },
+      backgroundImage: {
+        justice: 'url("/justica.jpg")',
+      },
     },
   },
   plugins: [],
