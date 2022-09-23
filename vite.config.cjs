df1746482112a452dc8f0e2558b6b0c2057320a9
João Pedro <jpedro1580@gmail.com>
2022-09-22 23:11:02 -0300

    Reconfigure vite for production build

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        thanks: resolve(__dirname, 'thanks.html'),
+      },
+    },
+  },
 });
